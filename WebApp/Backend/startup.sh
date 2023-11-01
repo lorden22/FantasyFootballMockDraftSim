d976897ccb3bcb0a58b1e7d3aafe5c1cd31a6437
Bryan Lorden <bryan.lorden22@gmail.com>
2023-11-01 10:19:31 -0500

    fix bug running database in the docker container

--- WebApp/Backend/startup.sh
+++ WebApp/Backend/startup.sh
@@ -15,7 +15,6 @@ mysql -h localhost -P 3306 -u $USER --password="$PASSWORD" -e "CREATE DATABASE $
 mysql -h localhost -P 3306 -u $USER --password="$PASSWORD" $DATABASE < /mockDraft.sql
 mysql -h localhost -P 3306 -u $USER --password="$PASSWORD" -e "SET PASSWORD FOR '$USER'@'localhost' = PASSWORD('$PASSWORD')"
 
-
 #rap "mysqladmin -h localhost -P 3306 -u $USER --password=\"$PASSWORD\" shutdown" EXIT
 
 java -jar /app.jar
